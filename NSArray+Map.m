21677205f1d0aff542d474f857e897eb8b00b1ad
Tj <tfallon@mail.depaul.edu>
2015-08-18 11:12:44 -0400

    Some style updates, warning fixes. Mainly adding path fetching for an arbitrary NSBundle. Not ready to call this done, but branching and pushing for now.

--- NSArray+Map.m
+++ NSArray+Map.m
@@ -6,11 +6,14 @@
 
 @implementation NSArray (Map)
 
-- (NSArray *)rnfs_mapObjectsUsingBlock:(id (^)(id obj, NSUInteger idx))block {
+- (NSArray *)rnfs_mapObjectsUsingBlock:(id (^)(id obj, NSUInteger idx))block
+{
   NSMutableArray *result = [NSMutableArray arrayWithCapacity:[self count]];
+
   [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
     [result addObject:block(obj, idx)];
   }];
+
   return result;
 }
 
