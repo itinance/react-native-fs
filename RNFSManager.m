33d637e42e17900b41133d05bfbacd31d1a1ac4f
Kyle Corbitt <kyle@corbt.com>
2016-03-18 10:53:00 -0700

    new getFSInfo command

--- RNFSManager.m
+++ RNFSManager.m
@@ -254,6 +254,32 @@ RCT_EXPORT_METHOD(pathForBundle:(NSString *)bundleNamed
     }
 }
 
+RCT_EXPORT_METHOD(getFSInfo:(RCTResponseSenderBlock)callback)
+{
+    unsigned long long totalSpace = 0;
+    unsigned long long totalFreeSpace = 0;
+    
+    __autoreleasing NSError *error = nil;
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSDictionary *dictionary = [[NSFileManager defaultManager] attributesOfFileSystemForPath:[paths lastObject] error: &error];
+    
+    if (dictionary) {
+        NSNumber *fileSystemSizeInBytes = [dictionary objectForKey: NSFileSystemSize];
+        NSNumber *freeFileSystemSizeInBytes = [dictionary objectForKey:NSFileSystemFreeSize];
+        totalSpace = [fileSystemSizeInBytes unsignedLongLongValue];
+        totalFreeSpace = [freeFileSystemSizeInBytes unsignedLongLongValue];
+        
+        callback(@[[NSNull null],
+                   @{
+                       @"totalSpace": [NSNumber numberWithUnsignedLongLong:totalSpace],
+                       @"freeSpace": [NSNumber numberWithUnsignedLongLong:totalFreeSpace]
+                       }
+                   ]);
+    } else {
+        callback(@[error, [NSNull null]]);
+    }
+}
+
 - (NSNumber *)dateToTimeIntervalNumber:(NSDate *)date
 {
   return @([date timeIntervalSince1970]);
