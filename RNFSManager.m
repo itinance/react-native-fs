a39c22be81f0c1f2263dbe60f3cd6cfcc902d2ac
Samet Demir <demir.samet@hotmail.com>
2017-07-16 15:18:55 +0300

    add read method with lenght and position params

--- RNFSManager.m
+++ RNFSManager.m
@@ -261,6 +261,51 @@ RCT_EXPORT_METHOD(readFile:(NSString *)filepath
   resolve(base64Content);
 }
 
+RCT_EXPORT_METHOD(read:(NSString *)filepath
+                  length: (NSInteger *)length
+                  position: (NSInteger *)position
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:filepath];
+    
+    if (!fileExists) {
+        return reject(@"ENOENT", [NSString stringWithFormat:@"ENOENT: no such file or directory, open '%@'", filepath], nil);
+    }
+    
+    NSError *error = nil;
+    
+    NSDictionary *attributes = [[NSFileManager defaultManager] attributesOfItemAtPath:filepath error:&error];
+    
+    if (error) {
+        return [self reject:reject withError:error];
+    }
+    
+    if ([attributes objectForKey:NSFileType] == NSFileTypeDirectory) {
+        return reject(@"EISDIR", @"EISDIR: illegal operation on a directory, read", nil);
+    }
+    
+    // Open the file handler.
+    NSFileHandle *file = [NSFileHandle fileHandleForReadingAtPath:filepath];
+    if (file == nil) {
+        return reject(@"EISDIR", @"EISDIR: Could not open file for reading", nil);
+    }
+    
+    // Seek to the position if there is one.
+    [file seekToFileOffset: (int)position];
+    
+    NSData *content;
+    if ((int)length > 0) {
+        content = [file readDataOfLength: (int)length];
+    } else {
+        content = [file readDataToEndOfFile];
+    }
+    
+    NSString *base64Content = [content base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];
+    
+    resolve(base64Content);
+}
+
 RCT_EXPORT_METHOD(hash:(NSString *)filepath
                   algorithm:(NSString *)algorithm
                   resolver:(RCTPromiseResolveBlock)resolve
