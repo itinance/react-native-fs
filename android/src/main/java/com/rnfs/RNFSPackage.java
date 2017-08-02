fc4b4932d947e9781495eba4f58c05e86681f089
Filip Kinský <filip@filovo.net>
2017-08-02 15:59:04 +0200

    removed createJSModules method (RN 0.47 breaking change)
    see https://github.com/facebook/react-native/releases/tag/v0.47.0 for explanation

--- android/src/main/java/com/rnfs/RNFSPackage.java
+++ android/src/main/java/com/rnfs/RNFSPackage.java
@@ -18,11 +18,6 @@ public class RNFSPackage implements ReactPackage {
   }
   
   @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
-
-  @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Arrays.<ViewManager>asList();
   }
