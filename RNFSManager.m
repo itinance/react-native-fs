82980c30a2d07083d0b74e7496d445856a01f02d
Leonid Shevtsov <leonid@shevtsov.me>
2019-10-10 08:36:32 +0300

    Add conditional comments around methods not supported in Mac Catalyst

--- RNFSManager.m
+++ RNFSManager.m
@@ -708,6 +708,8 @@ RCT_EXPORT_METHOD(getFSInfo:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromise
 }
 
 
+// [PHAsset fetchAssetsWithALAssetURLs] is deprecated and not supported in Mac Catalyst
+#if !TARGET_OS_UIKITFORMAC
 /**
  * iOS Only: copy images from the assets-library (camera-roll) to a specific path, asuming
  * JPEG-Images.
@@ -796,7 +798,10 @@ RCT_EXPORT_METHOD(copyAssetsFileIOS: (NSString *) imageUri
         }
     }];
 }
+#endif
 
+// [PHAsset fetchAssetsWithALAssetURLs] is deprecated and not supported in Mac Catalyst
+#if !TARGET_OS_UIKITFORMAC
 /**
  * iOS Only: copy videos from the assets-library (camera-roll) to a specific path as mp4-file.
  *
@@ -849,6 +854,7 @@ RCT_EXPORT_METHOD(copyAssetsVideoIOS: (NSString *) imageUri
 
   return resolve(destination);
 }
+#endif
 
 RCT_EXPORT_METHOD(touch:(NSString*)filepath
                   mtime:(NSDate *)mtime
