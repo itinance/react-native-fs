47d778dd6a596da5ae4d9f124ce187ce79099c45
leejunhui <leejunhui2015@gmail.com>
2021-04-07 10:14:34 +0800

    feat: iOS 端增加 readLine 实现

---
+++ LineByLineFileReader.h
@@ -0,0 +1,18 @@
+//
+//  LineByLineFileReader.h
+//
+//  Copyright (c) 2012 Alex Moffat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LineByLineFileReader : NSObject
+
+@property (nonatomic, strong) NSInputStream *inputStream;
+
+// Process the file identified by path calling block with each line to be processed or any
+// error. Either line or error will be non nil.
+- (void)processFile:(NSString *)path withEncoding:(NSStringEncoding)fileEncoding withLineCount:(NSInteger)lineCount usingBlock:(void (^)(NSString *line, NSError *error))block;
+
+- (void)closeStream;
+@end
