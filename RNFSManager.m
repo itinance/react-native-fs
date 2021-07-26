9eb5146e37956b7dfe0cce0b8fee79c8e40e0064
Fred Westling <fred@westling.io>
2021-07-26 12:53:06 +1000

    Made read() use 64-bit position rather than 32 bit to support large files

--- RNFSManager.m
+++ RNFSManager.m
@@ -323,7 +323,7 @@ RCT_EXPORT_METHOD(read:(NSString *)filepath
     }
 
     // Seek to the position if there is one.
-    [file seekToFileOffset: (int)position];
+    [file seekToFileOffset: (long)position];
 
     NSData *content;
     if ((int)length > 0) {
